22a0bd4668d74f18da444280bd431afb4adb361e
Korawit Prutsachainimmit <korawit.p@gmail.com>
2020-01-16 12:09:34 +0700

    Add project files.

---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("serialno")]
+        public string SerialNo { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+
+        [BsonElement("photo")]
+        public string PhotoURL { get; set; }
+    }
+}
