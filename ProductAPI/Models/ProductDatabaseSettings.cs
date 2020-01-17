652ef88b6a5da8e2d1eae4eeb214e2f8b7704b70
Korawit Prutsachainimmit <korawit.p@gmail.com>
2020-01-17 12:52:47 +0700

    Updated config

--- ProductAPI/Models/ProductDatabaseSettings.cs
+++ ProductAPI/Models/ProductDatabaseSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ProductAPI.Models
+﻿namespace ProductAPI.Models
 {
     public class ProductDatabaseSettings : IProductDatabaseSettings
     {
--- ProductAPI/Services/ProductService.cs
+++ ProductAPI/Services/ProductService.cs
@@ -1,9 +1,7 @@
 ﻿using MongoDB.Driver;
 using ProductAPI.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProductAPI.Services
 {
